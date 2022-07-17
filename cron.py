4dec4b22f9f1f42f47cb8a9614e5c0e53d0c1091
Nathan Schulzke <nschulzke@gmail.com>
2022-07-17 16:42:37 -0600

    Make the directory path for the cron job be derived automatically from the dirname

--- cron.py
+++ cron.py
@@ -1,9 +1,11 @@
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
 from crontab import CronTab
 
 with CronTab(user=True) as cron:
     cron.remove_all()
 
-    job = cron.new(command='python /home/nathan/personal/cast/plex.py "Kitchen Display" "Christmas Music"')
-    job.dow.every('SUN', 'MON', 'TUE')
-    job.hour.on(16)
-    job.minute.on(17)
+    job = cron.new(command=f'python {dir_path}/plex.py "Kitchen Display" "Christmas Music"')
+    job.dow.on('SUN', 'MON', 'TUE')
+    job.hour.every(16)
+    job.minute.on(41)
